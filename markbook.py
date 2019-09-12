72e857cfe2ae533d225de443a7f9b6490baa781c
josh <joshua.chuang21@ycdsbk12.ca>
2019-09-12 13:00:45 -0400

    update

--- markbook.py
+++ markbook.py
@@ -27,20 +27,27 @@ from typing import Dict
 # points: float
 
 
-def create_student(first_name: str, last_name: str, gender: str,
-                   image: str, student_number: int, grade: int, email: str) -> Dict:
-    """Creates a student dictionary""""
-    return {}
+def create_student(first_name: str, last_name: str,
+                   gender: str, image: str,
+                   student_number: int, grade: int,
+                   email: str) -> Dict:
+    """Creates a student dictionary"""
+    return {first_name, last_name,  gender, image,
+            student_number, grade, email}
 
 
-def create_classroom(course_code: str, course_name: str, period: int, teacher: str) -> Dict:
+def create_classroom(course_code: str,
+                     course_name: str, period: int,
+                     teacher: str) -> Dict:
     """Creates a classroom dictionary"""
-    return {}
+    return {course_code, course_name, period, teacher}
 
 
-def create_assignment(due: str, name: str, points: float):
-    """Creates an assignment represented as a dictionary"""
-    return {}
+def create_assignment(due: str, name: str,
+                      points: float):
+    """Creates an assignment represented as a
+    dictionary"""
+    return {due, name, points}
 
 
 def calculate_average_mark(student: Dict) -> float:
@@ -48,4 +55,6 @@ def calculate_average_mark(student: Dict) -> float:
     return 0
 
 
-print(create_classroom("ICS4U", "Computer Science 12", 3, "Mr. Gallo"))
+print(create_student("Joshua", "Chuang", "Male",
+                     "Image", 6479299892, 11,
+                     "joshthegreatsayshi@gmail.com"))
