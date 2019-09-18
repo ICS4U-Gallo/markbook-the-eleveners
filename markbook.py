edf09b5e1af4397dc1776abba044cb8f305fdef1
josh <joshua.chuang21@ycdsbk12.ca>
2019-09-18 13:31:26 -0400

    user interface

--- markbook.py
+++ markbook.py
@@ -37,7 +37,6 @@ def create_student(first_name: str, last_name: str,
             student_number, "grade": grade, "email": email}
 
 
-
 def create_assignment(name: str, due: str, points: int) -> Dict:
     """Creates an assignment represented as a dictionary
 
@@ -51,22 +50,19 @@ def create_assignment(name: str, due: str, points: int) -> Dict:
     return {"name": name, "due": due, "points": points}
 
 
-
 def create_classroom(course_code: str, course_name: str,
                      period: int, teacher: str) -> Dict:
     """Creates a classroom dictionary"""
 
     return {"course_code": course_code, "course_name": course_name,
-            "period": period, "teacher": teacher, 
+            "period": period, "teacher": teacher,
             "student_list": [], "assignment_list": []}
-
 
 
 def calculate_average_mark(student: Dict) -> float:
     """Calculates the average mark of a student"""
 
     return sum(student["marks"]) / len(student["marks"])
-
 
 
 def add_student_to_classroom(student, classroom):
@@ -76,8 +72,8 @@ def add_student_to_classroom(student, classroom):
         student: Student dict
         classroom: The classroom to add the student to
     """
-    classroom["student_list"].append("{} {}".format(student["first_name"], student["last_name"]))
-
+    classroom["student_list"].append("{} {}".format(
+        student["first_name"], student["last_name"]))
 
 
 def remove_student_from_classroom(student: Dict, classroom: Dict):
@@ -87,8 +83,8 @@ def remove_student_from_classroom(student: Dict, classroom: Dict):
         student: The student to be removed
         classroom: the class from which the student will be removed.
     """
-    classroom["student_list"].remove("{} {}".format(student["first_name"], student["last_name"]))
-
+    classroom["student_list"].remove("{} {}".format(
+        student["first_name"], student["last_name"]))
 
 
 def edit_student(student: Dict, **kwargs: Dict):
@@ -102,3 +98,7 @@ def edit_student(student: Dict, **kwargs: Dict):
     """
     for data in kwargs.keys():
         student[data] = kwargs[data]
+
+
+while True:
+    int(input("\n Create Student[0]\n Create Assignment[1]\n Create Classroom[2]\n Calculate Average Mark[3]\n Add Student To Classroom[4]\n Remove Student From Classroom[5]\n Edit Student[6]"))
